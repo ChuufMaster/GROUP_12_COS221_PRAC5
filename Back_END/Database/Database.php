c198bbf1b461526ff7cab2145d906abc3fc7b7ed
ChuufMaster <ivanhorak20@gmail.com>
2023-06-05 15:17:31 +0200

    Added more functionality
    - get_random, returns a random selection of rows from the specified table
    - get_by_conditions, returns all the rows up to the limit of the specified table with the conditions
    - get_by_winery, returns all the wines to the limit of the specified winery name
    - add_images, adds images to the wines table. look at example.json to see the usage

--- Back_END/Database/Database.php
+++ Back_END/Database/Database.php
@@ -1,130 +1,154 @@
 <?php
 
-class Database {
+class Database
+{
     private $connection; // Database connection
-    
-    public static function instance($host, $username, $password, $database){
+
+    public static function instance($host, $username, $password, $database)
+    {
         static $instance = null;
-        if($instance === null){
+        if ($instance === null)
+        {
             $instance = new Database($host, $username, $password, $database);
         }
         return $instance;
     }
 
     // Constructor
-    public function __construct($host, $username, $password, $database) {
+    public function __construct($host, $username, $password, $database)
+    {
         $this->connection = $this->connect($host, $username, $password, $database);
     }
-    
+
     // Connect to the database
-    private function connect($host, $username, $password, $database) {
+    private function connect($host, $username, $password, $database)
+    {
         $connection = mysqli_connect($host, $username, $password, $database);
-        
+
         // Check connection
-        if (mysqli_connect_errno()) {
+        if (mysqli_connect_errno())
+        {
             die('Failed to connect to MySQL: ' . mysqli_connect_error());
         }
-        
+
         return $connection;
     }
-    
+
     // Execute an SQL query
-    public function executeQuery($query) {
+    public function executeQuery($query)
+    {
         $result = mysqli_query($this->connection, $query);
-        
+
         // Check for errors
-        if (!$result) {
+        if (!$result)
+        {
             return mysqli_error($this->connection);
         }
-        
+
         return $result;
     }
-    
-    // Perform a SELECT query with JOIN operations. Tables and Columns have to be arrays even if it is just one.
-    public function select($tables, $columns, $joins = array(), $conditions = array(), $order = '', $limit = '') {
-        if(!is_array($columns))
-            $columns = array($columns);
-        if(!is_array($tables))
-            $tables = array($tables);
 
+    // Perform a SELECT query with JOIN operations. Tables and Columns have to be arrays even if it is just one.
+    public function select($tables, $columns, $joins = array(), $conditions = array(), $order = '', $limit = '')
+    {
+        if (!is_array($columns))
+            $columns = array($columns);
+        if (!is_array($tables))
+            $tables = array($tables);
+        if (!is_array($joins))
+            $joins = array($joins);
         $query = "SELECT " . implode(', ', $columns);
         $query .= " FROM " . implode(', ', $tables);
-        
-        if (!empty($joins)) {
+
+        if (!empty($joins))
+        {
             $query .= " " . implode(' ', $joins);
         }
-        
-        if (!empty($conditions)) {
+
+        if (!empty($conditions))
+        {
             $whereConditions = array();
-            foreach ($conditions as $column => $value) {
+            foreach ($conditions as $column => $value)
+            {
                 $escaped_value = mysqli_real_escape_string($this->connection, $value);
                 $whereConditions[] = "$column = '$escaped_value'";
             }
             $query .= " WHERE " . implode(' AND ', $whereConditions);
         }
-        
-        if (!empty($order)) {
+
+        if (!empty($order))
+        {
             $query .= " ORDER BY " . $order;
         }
-        
-        if (!empty($limit)) {
+
+        if (!empty($limit))
+        {
             $query .= " LIMIT " . $limit;
         }
-        
+
         return $this->executeQuery($query);
+        //return $query;
     }
-    
+
     // Perform an INSERT query. Columns has to be an array in the format (column => value)
-    public function insert($table, $data) {
+    public function insert($table, $data)
+    {
         $columns = implode(', ', array_keys($data));
         $values = implode("', '", array_values($data));
         $query = "INSERT INTO $table ($columns) VALUES ('$values')";
-        
+
         $this->executeQuery($query);
-        
+
         return mysqli_insert_id($this->connection);
     }
-    
+
     // Perform an UPDATE query
-    public function update($table, $columns, $conditions) {
-        if (!is_array($conditions)) {
+    public function update($table, $columns, $conditions)
+    {
+        if (!is_array($conditions))
+        {
             $conditions = array($conditions);
         }
-        
+
         $set = '';
-        foreach ($columns as $column => $value) {
+        foreach ($columns as $column => $value)
+        {
             $set .= "$column = '$value', ";
         }
         $set = rtrim($set, ', ');
-        
+
         $conditions = implode(' AND ', $conditions);
-        
+
         $query = "UPDATE $table SET $set WHERE $conditions";
-        
+
         return $this->executeQuery($query);
     }
-    
+
     // Perform a DELETE query
-    public function delete($table, $conditions) {
-        if (!is_array($conditions)) {
+    public function delete($table, $conditions)
+    {
+        if (!is_array($conditions))
+        {
             $conditions = array($conditions);
         }
-    
+
         $query = "DELETE FROM $table";
 
         $where_conditions = array();
-        foreach ($conditions as $column => $value) {
+        foreach ($conditions as $column => $value)
+        {
             $escaped_value = mysqli_real_escape_string($this->connection, $value);
             $where_conditions[] = "$column = '$escaped_value'";
         }
         $query .= " WHERE " . implode(' AND ', $where_conditions);
-        
-        
+
+
         return $this->executeQuery($query);
     }
-    
+
     // Close the database connection
-    public function close() {
+    public function close()
+    {
         mysqli_close($this->connection);
     }
 }
